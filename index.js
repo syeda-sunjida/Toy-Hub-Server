66d7b36d5cc06391e3015714c715242eb47dbd31
syeda-sunjida <nikitaferdous1999@gmail.com>
2023-05-23 23:01:08 +0600

    update

--- index.js
+++ index.js
@@ -88,6 +88,7 @@ async function run() {
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
+        //closing cancel
     }
 }
 run().catch(console.dir);
