448c03c302b2ea3cf04743dd7d26f49b1ad4e653
syeda-sunjida <nikitaferdous1999@gmail.com>
2023-05-23 23:24:37 +0600

    try comment removed

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
+        
         // await client.connect();
 
         const serviceCollection = client.db('toyHub').collection('toy');
