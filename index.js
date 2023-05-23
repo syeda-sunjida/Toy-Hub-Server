a51e2abc4630b65cdf6a317bc34a9f0db2040c58
syeda-sunjida <nikitaferdous1999@gmail.com>
2023-05-23 23:25:37 +0600

    try comment removed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ console.log(process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mhlikac.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
